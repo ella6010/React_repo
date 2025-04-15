e81c8624c05f29121f3e8279d30b4ce0c4e188a1
Ella <ella6010@gmail.com>
2025-04-16 02:00:52 +0900

    리액트 10일차 과제 scss

---
+++ src/App.jsx
@@ -0,0 +1,18 @@
+import './App.scss';
+import Content from './components/Content';
+import Header from './components/Header';
+
+function App() {
+  return (
+    <main>
+      <Header />
+      <section>
+        {contents.map((el) => (
+          <Content key={el.id} content={el} />
+        ))}
+      </section>
+    </main>
+  );
+}
+
+export default App;
