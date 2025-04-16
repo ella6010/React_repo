56b6ee2d34cb653d4597a9155dcbf77c8b1369d3
Ella <ella6010@gmail.com>
2025-04-16 15:51:17 +0900

    React styled-components 실습파일 생성 및 커밋

--- src/App.jsx
+++ src/App.jsx
@@ -25,14 +25,37 @@ const BigTextBigBlueButton = styled(BigBlueButton)`
   font-weight: 900;
 `;
 
+const PropsButton = styled.button`
+  background-color: ${props => props.backgroundColor || 'white'};
+  color: ${(props) => props.textColor || 'black'};
+  padding: ${(props) => props.padding || 0};
+  border-radius: ${(props) => props.radius || 0};
+  margin: ${(props) => props.margin || 0};
+  width: ${(props) => props.width || 'auto'};
+  font-size: ${(props) => props.fontSize || 'auto'};
+  font-weight: ${(props) => props.fontWeight || 400};
+`;
+
 function App() {
   return (
     <>
     <Container>
       <div>hello</div>
       <BlueButton>파란색 버튼</BlueButton> 
+      <PropsButton backgroundColor={'blue'} textColor="white" padding="10px" radius="4px"
+      margin="10px">파란색 버튼</PropsButton>
+
       <BigBlueButton>커다란 파란색 버튼</BigBlueButton>
+      <PropsButton backgroundColor={'blue'} textColor="white" padding="20px" radius="4px"
+      margin="10px" width="300px">커다란 파란색 버튼</PropsButton>
+
       <BigTextBigBlueButton>글자도 커다란 파란색 버튼</BigTextBigBlueButton>
+      <PropsButton backgroundColor={'red'} textColor="white" padding="20px" radius="4px"
+      margin="10px" width="300px" fontSize="30px" fontWeight={900}>
+      글자도 커다란 파란색 버튼</PropsButton>
+
+      {/* <PropsButton backgroundColor="green" textColor="yellow">Props 버튼</PropsButton>
+      <PropsButton backgroundColor="yellow" textColor="green">Props 버튼</PropsButton> */}
     </Container>  
     </>
   );
