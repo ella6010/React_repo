52d95703945f49e37d76d578cda7c937148f284b
Ella <ella6010@gmail.com>
2025-04-16 18:22:36 +0900

    React 11일차 과제

--- src/App.jsx
+++ src/App.jsx
@@ -1,24 +1,4 @@
-<<<<<<< HEAD
-import "./App.scss";
-import { contents } from "./assets/data/data";
-import Content from "./components/Content";
-import Header from "./components/Header";
 
-function App() {
-  return (
-    <main>
-      <Header />
-      <section>
-        {contents.map((el) => (
-          <Content key={el.id} content={el} />
-        ))}
-      </section>
-    </main>
-  );
-}
-
-export default App;
-=======
 import styled from "styled-components";
 // import "./App.scss";
 import { contents } from "./assets/data/data";
@@ -45,4 +25,3 @@ function App() {
 }
 
 export default App;
->>>>>>> 7b4c041 (React 11일차 과제)
