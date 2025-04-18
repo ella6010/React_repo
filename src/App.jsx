7a56483b7af2c7085668f32c1633c4428247aebc
Ella <ella6010@gmail.com>
2025-04-18 11:19:51 +0900

    250417_practice01 tailwind CSS 실습

--- src/App.jsx
+++ src/App.jsx
@@ -3,7 +3,7 @@ import './App.css'
 function App() {
   return (
     <div className='flex justify-center items-center h-screen'>
-      <div className='text-5xl text-[rgb(88,200,170)]'>안녕하세요</div>
+      <div className='text-5xl text-sky-600'>안녕하세요</div>
     </div>
   )
 }
