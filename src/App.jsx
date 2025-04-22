e83e1c06ef8075b4ed2af09233c6583efb61bb8b
Ella <ella6010@gmail.com>
2025-04-22 15:33:40 +0900

    250422_실습파일

--- src/App.jsx
+++ src/App.jsx
@@ -1,43 +1,77 @@
-import { legacy_createStore } from 'redux';
+import { applyMiddleware, combineReducers, legacy_createStore } from 'redux';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 
 //Action + 객체
-const increment = {
-  type: 'increment'
+const increment1 = {
+  type: 'increment1'
 }
 
 //Action - 객체
-const decrement = {
-  type: 'decrement'
+const decrement1 = {
+  type: 'decrement1'
+}
+
+//Action + 객체
+const increment2 = {
+  type: 'increment2'
+}
+
+//Action - 객체
+const decrement2 = {
+  type: 'decrement2'
 }
 
 //Reducer
-const counterReducer = (state = 0, action) => {
+const counter1Reducer = (state = 5, action) => {
   switch (action.type) {
-    case 'increment':
+    case 'increment1':
       return state + 1
-    case 'decrement':
+    case 'decrement1':
       return state - 1
     default:
       return state //기본상태 유지
   }
 }
 
+const counter2Reducer = (state = 0, action) => {
+  switch (action.type) {
+    case 'increment2':
+      return state + 1
+    case 'decrement2':
+      return state - 1
+    default:
+      return state //기본상태 유지
+  }
+}
+
+//여러개의 Reducer 사용
+const rootReducer = combineReducers({ counter1Reducer, counter2Reducer })
+
 //Store 상태 저장소 createStore = legacy_createStore 
-export const store = legacy_createStore(counterReducer)
+export const store = legacy_createStore(rootReducer, applyMiddleware(thunk)) //Redux-Thunk 사용준비
 
 
 function App() {
-  const counter = useSelector(state => state)
-  console.log(counter)
+  const counter1 = useSelector((state) => state.counter1Reducer)
+  const counter2 = useSelector((state) => state.counter2Reducer)
   const dispatch = useDispatch() //Dispatch
-  dispatch(increment)
   return (
     <>
-      <div>Counter : { counter }</div>
-      <button onClick={() => dispatch(increment)}>+</button>
-      <button onClick={() => dispatch(decrement)}>-</button>
+      <div>Counter1 : { counter1 }</div>
+      <button onClick={() => dispatch((dispatch) => {
+        setTimeout(() => {
+          dispatch(increment1)
+        }, 1000)
+      })}>+</button>
+      <button onClick={() => dispatch((dispatch) => {
+        setTimeout(() => {
+          dispatch(decrement1)
+        }, 1000)
+      })}>-</button>
+      <div>Counter2 : { counter2 }</div>
+      <button onClick={() => dispatch(increment2)}>+</button>
+      <button onClick={() => dispatch(decrement2)}>-</button>
     </>
   );
 }
