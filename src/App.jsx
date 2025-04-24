d29a78612f4b403ab46e39ff02aaa0f9cca0bb9f
Ella <ella6010@gmail.com>
2025-04-24 17:09:45 +0900

    [React최적화] React 17일차 과제01

---
+++ src/App.jsx
@@ -0,0 +1,17 @@
+import React, { Suspense, lazy } from 'react';
+import './App.css';
+
+const TestComponent = lazy(() => import('./TestComponent'));
+
+function App() {
+  return (
+    <div className="App">
+      <h1>lazy와 Suspense<br/> 사용해보기</h1>
+      <Suspense fallback={<h2>로딩 중...</h2>}>
+        <TestComponent />
+      </Suspense>
+    </div>
+  );
+}
+
+export default App;
